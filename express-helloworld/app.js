4f9f1fb7be80a83f5bfe4a13dceca10df17d2ffe
cmmatei <cmmatei@hotmail.com>
2020-04-30 12:14:39 +0200

    comit

--- express-helloworld/app.js
+++ express-helloworld/app.js
@@ -5,6 +5,10 @@ app.get('/', function (req, res) {
   res.send('Hello World!\n');
 });
 
+app.get('/saturne', function (req, res) {
+  res.send('Hello saturne!\n');
+});
+
 app.listen(8080, function () {
   console.log('Example app listening on port 8080!');
 });
